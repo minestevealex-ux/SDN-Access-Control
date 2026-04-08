cea6eb78816889b523d4ad4fe54d5af5d31b2404
minestevealex-ux <minestevealex@gmail.com>
2026-04-08 09:40:24 +0530

    Create access_control.py

---
+++ access_control.py
@@ -0,0 +1,145 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet, ethernet, ipv4, ether_types
+
+
+class AccessControl(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(AccessControl, self).__init__(*args, **kwargs)
+        self.mac_to_port = {}
+
+        # 🔥 BLOCK THIS HOST (h3)
+        self.blocked_ips = ['10.0.0.3']
+
+    # 🔥 Table-miss rule (VERY IMPORTANT)
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        if buffer_id:
+            mod = parser.OFPFlowMod(
+                datapath=datapath,
+                buffer_id=buffer_id,
+                priority=priority,
+                match=match,
+                instructions=inst
+            )
+        else:
+            mod = parser.OFPFlowMod(
+                datapath=datapath,
+                priority=priority,
+                match=match,
+                instructions=inst
+            )
+
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+        # Ignore LLDP
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            return
+
+        src = eth.src
+        dst = eth.dst
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+
+        # 🔁 Learn MAC
+        self.mac_to_port[dpid][src] = in_port
+
+        # 🔐 FIREWALL LOGIC
+        ipv4_pkt = pkt.get_protocol(ipv4.ipv4)
+        if ipv4_pkt:
+            src_ip = ipv4_pkt.src
+            dst_ip = ipv4_pkt.dst
+
+            if src_ip in self.blocked_ips or dst_ip in self.blocked_ips:
+                self.logger.info("🚫 BLOCKED: %s → %s", src_ip, dst_ip)
+
+                # 🔥 Install DROP rule
+                match = parser.OFPMatch(
+                    eth_type=ether_types.ETH_TYPE_IP,
+                    ipv4_src=src_ip,
+                    ipv4_dst=dst_ip
+                )
+                self.add_flow(datapath, 10, match, [])
+
+                return
+
+        # ✅ ALLOWED TRAFFIC
+        self.logger.info("✅ ALLOWED: %s → %s", src, dst)
+
+        # 🔁 Forwarding (learning switch)
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # Install flow rule
+        if out_port != ofproto.OFPP_FLOOD:
+            if ipv4_pkt:
+                match = parser.OFPMatch(
+                    in_port=in_port,
+                    eth_src=src,
+                    eth_dst=dst,
+                    eth_type=ether_types.ETH_TYPE_IP,
+                    ipv4_src=ipv4_pkt.src,
+                    ipv4_dst=ipv4_pkt.dst
+                )
+            else:
+                match = parser.OFPMatch(
+                    in_port=in_port,
+                    eth_src=src,
+                    eth_dst=dst
+                )
+
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+
+        # 🔥 Send packet immediately
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=data
+        )
+
+        datapath.send_msg(out)
